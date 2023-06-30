171f4a195ea76437ccfd89127f0d2362efb32b66
KIMWOOBIN <woodol4540@naver.com>
2023-07-01 02:23:47 +0900

    Day0701

---
+++ Assets/MovePlayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float speed = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxisRaw("Horizontal") != 0)
+        {
+
+        }
+    }
+}
