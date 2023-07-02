7f1ab39bb3cba7c3aee0102fbf3daee2c9dd2ff0
KIMWOOBIN <woodol4540@naver.com>
2023-07-02 13:26:28 +0900

    Day0702

--- Assets/Scripts/PlayerCtl.cs
+++ Assets/Scripts/PlayerCtl.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class PlayerCtl : MonoBehaviour
 {
-    // 걷기
+    // 걷기 제어
     public float speed;
     private Vector3 vector;
 
-    // 뛰기
+    // 뛰기 제어
     public float runSpeed;
     private float applyRunSpeed;
     private bool applyRunFlag = false;
@@ -20,43 +20,59 @@ public class PlayerCtl : MonoBehaviour
     // 중복 키 제어
     private bool canMove = true;
 
+    // 애니메이터 제어
+    private Animator animator;
+
     // Start is called before the first frame update
-    void Start()
+    void Start() 
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     IEnumerator MoveCoroutine() // 중복 키 제어
     {
-        if (Input.GetKey(KeyCode.LeftShift))
+        while(Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0)
         {
-            applyRunSpeed = runSpeed;
-            applyRunFlag = true;
-        }
-        else
-        {
-            applyRunSpeed = 0;
-            applyRunFlag = false;
-        }
-        vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
-        while (currentWalkCount < walkCount)
-        {
-            if (vector.x != 0)
+            if (Input.GetKey(KeyCode.LeftShift))
             {
-                transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
+                applyRunSpeed = runSpeed;
+                applyRunFlag = true;
             }
-            else if (vector.y != 0)
+            else
             {
-                transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
+                applyRunSpeed = 0;
+                applyRunFlag = false;
             }
-            if (applyRunFlag)
+            vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+            // 애니메이션 설정
+            if(vector.x != 0)
             {
+                vector.y = 0;
+            }
+            animator.SetFloat("DirX", vector.x);
+            animator.SetFloat("DirY", vector.y);
+            animator.SetBool("Walking", true);
+
+            while (currentWalkCount < walkCount)
+            {
+                if (vector.x != 0)
+                {
+                    transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
+                }
+                else if (vector.y != 0)
+                {
+                    transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
+                }
+                if (applyRunFlag)
+                {
+                    currentWalkCount++;
+                }
                 currentWalkCount++;
+                yield return new WaitForSeconds(0.01f);
             }
-            currentWalkCount++;
-            yield return new WaitForSeconds(0.01f);
+            currentWalkCount = 0;
         }
-        currentWalkCount = 0;
+        animator.SetBool("Walking", false);
         canMove = true;
     }
 
