3b35660b8986e7e580b881c546ad7c9250e8243b
KIMWOOBIN <woodol4540@naver.com>
2023-07-05 08:40:34 +0900

    Day0705

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public GameObject target; // 카메라가 따라갈 대상
+    public float moveSpeed; // 카메라 속도
+    private Vector3 targetPosition; // 대상 현재 위치 값
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target.gameObject != null)
+        {
+            targetPosition.Set(target.transform.position.x, target.transform.position.y, this.transform.position.z); // this를 쓰는 이유 카메라가 타켓보다 더 뒤에 있어야 하기 때문에
+            this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, moveSpeed * Time.deltaTime); // 1초에 moveSpeed만큼 이동
+        }
+    }
+}
