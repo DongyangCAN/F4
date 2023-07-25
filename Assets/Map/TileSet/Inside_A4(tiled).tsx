08755af6e78a0694ca0d6e44379dfeded99c47a5
manlevnewbie <68420534+manlevnewbie@users.noreply.github.com>
2023-07-25 15:49:22 +0900

    Delete Assets/Map directory

--- Assets/Map/TileSet/Inside_A4(tiled).tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="Inside_A4(tiled)" tilewidth="48" tileheight="48" tilecount="240" columns="16">
- <image source="../../K-study/Tiled/Inside_A4(tiled).png" width="768" height="720"/>
-</tileset>
