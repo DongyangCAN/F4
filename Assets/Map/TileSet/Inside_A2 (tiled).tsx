55535d3f0f3bc154b5c885ece907f5d1ef3499be
KIMWOOBIN <woodol4540@naver.com>
2023-07-25 15:00:53 +0900

    Day0725

---
+++ Assets/Map/TileSet/Inside_A2 (tiled).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Inside_A2 (tiled)" tilewidth="48" tileheight="48" tilecount="192" columns="16">
+ <image source="../../K-study/Tiled/Inside_A2 (tiled).png" width="768" height="576"/>
+</tileset>
