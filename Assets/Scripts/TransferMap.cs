43f2ab76511daa60f0ada5e89e432a6b7420f217
KIMWOOBIN <woodol4540@naver.com>
2023-08-20 16:19:22 +0900

    Day0820

--- Assets/Scripts/TransferMap.cs
+++ Assets/Scripts/TransferMap.cs
@@ -13,105 +13,65 @@ public class TransferMap : MonoBehaviour
     private CameraManager theCamera;
     private FadeManager theFade;
     private OrderManager theOrder;
-    public Animator anim_1;
-    public Animator anim_2;
-    public int door_count;
     [Tooltip("UP, DOWN, LEFT, RIGHT")]
     public string direction; // 캐릭터가 바라보고 있는 방향
     private Vector2 vector; // getfloat("dirX")
-    [Tooltip("문O : true, 문X : false")]
-    public bool door; // 문이 있나 없나
-    void Start()
+    private void OnTriggerStay2D(Collider2D collision)
     {
         theCamera = FindObjectOfType<CameraManager>();
         thePlayer = FindObjectOfType<PlayerManager>();
         theFade = FindObjectOfType<FadeManager>();
         theOrder = FindObjectOfType<OrderManager>();
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (!door)
+        if (collision.gameObject.name == "Player")
         {
-            if (collision.gameObject.name == "Player(Main)")
+            vector.Set(thePlayer.animator.GetFloat("DirX"), thePlayer.animator.GetFloat("DirY"));
+            switch (direction)
             {
-                StopAllCoroutines();
-                StartCoroutine(TransforCoroutine());
-            }
-        }
-    }
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (door)
-        {
-            if (collision.gameObject.name == "Player(Main)")
-            {
-                if (Input.GetKeyDown(KeyCode.Z))
-                {
-                    vector.Set(thePlayer.animator.GetFloat("DirX"), thePlayer.animator.GetFloat("DirY"));
-                    switch (direction)
+                case "UP":
+                    if (vector.y == 1f)
                     {
-                        case "UP":
-                            if(vector.y == 1f)
-                            {
-                                StopAllCoroutines();
-                                StartCoroutine(TransforCoroutine());
-                            }
-                            break;
-                        case "DOWN":
-                            if (vector.y == -1f)
-                            {
-                                StopAllCoroutines();
-                                StartCoroutine(TransforCoroutine());
-                            }
-                            break;
-                        case "RIGHT":
-                            if (vector.x == 1f)
-                            {
-                                StopAllCoroutines();
-                                StartCoroutine(TransforCoroutine());
-                            }
-                            break;
-                        case "LEFT":
-                            if (vector.x == -1f)
-                            {
-                                StopAllCoroutines();
-                                StartCoroutine(TransforCoroutine());
-                            }
-                            break;
-                        default:
-                            StopAllCoroutines();
-                            StartCoroutine(TransforCoroutine());
-                            break;
+                        StopAllCoroutines();
+                        StartCoroutine(TransforCoroutine());
                     }
-                }
+                    break;
+                case "DOWN":
+                    if (vector.y == -1f)
+                    {
+                        StopAllCoroutines();
+                        StartCoroutine(TransforCoroutine());
+                    }
+                    break;
+                case "RIGHT":
+                    if (vector.x == 1f)
+                    {
+                        StopAllCoroutines();
+                        StartCoroutine(TransforCoroutine());
+                    }
+                    break;
+                case "LEFT":
+                    if (vector.x == -1f)
+                    {
+                        StopAllCoroutines();
+                        StartCoroutine(TransforCoroutine());
+                    }
+                    break;
+                default:
+                    StopAllCoroutines();
+                    StartCoroutine(TransforCoroutine());
+                    break;
             }
+
         }
+
     }
     IEnumerator TransforCoroutine()
     {
         theOrder.PreLoadCharacter();
         theOrder.NotMove();
         theFade.FadeOut();
-        if (door)
-        {
-            anim_1.SetBool("Open", true);
-            if (door_count == 2)
-            {
-                anim_2.SetBool("Open", true);
-            }
-        }
         yield return new WaitForSeconds(0.5f);
-        theOrder.SetTransparent("Player(Main)");
-        if (door)
-        {
-            anim_1.SetBool("Open", false);
-            if (door_count == 2)
-            {
-                anim_2.SetBool("Open", false);
-            }
-        }
         yield return new WaitForSeconds(0.5f);
-        theOrder.SetUnTransparent("Player(Main)");
+        theOrder.SetUnTransparent("Player");
         thePlayer.currentMapName = transferMapName;
         theCamera.SetBound(targetBound);
         theCamera.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, theCamera.transform.position.z);
