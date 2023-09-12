9d83f3628e4708d7ac7625cbde98456d5b30eb8a
KROOKIMWOOBIN <woodol4540@naver.com>
2023-09-12 14:09:07 +0900

    Day0912

---
+++ Assets/Scripts/dialogue_2_1.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogue_2_1 : MonoBehaviour
+{
+    [SerializeField]
+    public Dialogue dialogue;
+    private DialogueManager theDM;
+    private bool eventCheck = false;
+    private bool flag;
+    void Start()
+    {
+        theDM = FindObjectOfType<DialogueManager>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Player" && flag == false)
+        {
+            eventCheck = true;
+            flag = true;
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z) && eventCheck == true)
+        {
+            theDM.ShowDialogue(dialogue);
+            eventCheck = false;
+        }
+    }
+}
